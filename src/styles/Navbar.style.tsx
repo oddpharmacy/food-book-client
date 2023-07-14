b2c351dcea413454adfbb165901cdeaf160a3b92
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-14 08:11:43 +0800

    style: add styles for lang options

--- src/styles/Navbar.style.tsx
+++ src/styles/Navbar.style.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
+import LanguageIcon from "@mui/icons-material/Language";
 
 export const NavbarWrapper = styled.header`
   width: 100%;
@@ -72,5 +73,80 @@ export const NavLink = styled(Link)`
 
   &:hover {
     text-decoration: underline;
+  }
+`;
+
+export const NavLanguageIcon = styled(LanguageIcon)`
+  font-size: inherit;
+
+  &:active {
+    transform: translate(1px, 1px);
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: gray;
+  }
+`;
+
+export const NavDropdownWrapper = styled.div`
+  position: relative;
+`;
+
+interface DropdownProps {
+  showDropdown: boolean;
+}
+
+export const NavDropdown = styled.div<DropdownProps>`
+  display: ${(props) => (props.showDropdown ? "block" : "none")};
+  position: absolute;
+  top: 100%;
+  left: 0%;
+  right: 0;
+  z-index: 1;
+  line-height: 0;
+
+  &:before {
+    content: "";
+    position: absolute;
+    border: solid transparent;
+    border-bottom-color: white;
+    border-width: 11px;
+    top: -10px;
+    transform: translateX(-5%);
+    z-index: 1;
+  }
+`;
+
+export const NavButtonWrapper = styled.div`
+  background: white;
+  position: relative;
+  width: 100px;
+  top: 12px;
+  left: -200%;
+  padding: 1.2rem 0;
+  border-radius: 10px;
+
+  @media (max-width: 768px) {
+    width: 40px;
+    left: -65%;
+  }
+`;
+
+export const NavLangButton = styled.button`
+  display: block;
+  margin: 0 auto;
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  font-size: 1.5rem;
+
+  &:hover {
+    background-color: gray;
+    color: white;
+  }
+
+  &:first-of-type {
+    margin-bottom: 1rem;
   }
 `;
