d64f8f9eaed46867a5160da9c5a455330d270d02
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-14 08:12:36 +0800

    feat: init cypress home page tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe("home page", () => {
+  beforeEach(() => {
+    cy.visit(" http://127.0.0.1:5173/");
+  });
+
+  context("language", () => {
+    it.only("should change languages successfully", () => {
+      cy.getByData("lang-button").click();
+      cy.getByData("lang-bm").click();
+      cy.getByData("header-home").should("exist").contains("Utama");
+      cy.getByData("footer-copyright")
+        .should("exist")
+        .contains("Hak Cipta Terpelihara");
+    });
+  });
+
+  context("navigation", () => {
+    it("navigates to login", () => {
+      cy.getByData("header-logo").find("a").eq(3).click();
+      cy.location("pathname").should("eq", "/login");
+    });
+  });
+});
