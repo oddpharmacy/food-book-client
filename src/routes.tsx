eddc41028c02feb675525ca3e89d3c79890c2365
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-16 05:38:18 +0800

    feat: include route to search results

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
+import SearchResults from "./components/SearchResults";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes: React.FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/search/:query" element={<SearchResults />} />
     </Routes>
   );
 };
