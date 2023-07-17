13915b22d09d15b5fa732922aa01580b69943f6f
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-18 05:08:40 +0800

    style: search results

---
+++ src/styles/SearchResults.style.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const SearchResultsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+`;
+
+export const SearchTitle = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+  margin-top: 1.5rem;
+`;
