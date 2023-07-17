a93750e2f62703b61fd0238a4d936f7487c221b8
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-18 05:48:33 +0800

    feat: search results test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,10 +6,10 @@ describe("home page", () => {
   });
 
   context("language", () => {
-    it.only("should change languages successfully", () => {
+    it("should change languages successfully", () => {
       cy.getByData("lang-button").click();
       cy.getByData("lang-bm").click();
-      cy.getByData("header-home").should("exist").contains("Utama");
+      cy.getByData("header-home").contains("Utama");
       cy.getByData("footer-copyright")
         .should("exist")
         .contains("Hak Cipta Terpelihara");
@@ -20,6 +20,13 @@ describe("home page", () => {
     it("navigates to login", () => {
       cy.getByData("header-logo").find("a").eq(3).click();
       cy.location("pathname").should("eq", "/login");
+    });
+    it("navigates to the search results", () => {
+      cy.getByData("header-logo").find("a").eq(0).click();
+      cy.getByData("search-box").type("japanese");
+      cy.getByData("search-button").click();
+      cy.location("pathname").should("eq", "/search/japanese");
+      cy.get("h1").contains("Showing results for");
     });
   });
 });
